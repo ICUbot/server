102cc06ba559095f1f60bec31cab0c38341c71e2
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2015-11-14 18:09:47 -0500

    Added captured tweet

--- server.js
+++ server.js
@@ -64,6 +64,38 @@ app.get('/intruder', function(req, res){
   })
 });
 
+app.get('/captured', function(req, res){
+  // auth keys
+  var T = new Twit({
+      consumer_key:         '7NcliyXkErrRqXaPkJU84jEQn'
+    , consumer_secret:      '1Q2KcBfhbw4xUuTqvAre2GBXT0OYhd3lIZa0wmuDR7Nie8fKgs'
+    , access_token:         '622419647-a60CFvSpdbjyGV0JjcqZL6EX4s6vMi8oNI9SLoKm'
+    , access_token_secret:  'kemKmdVFRK1Bm34cO6c55pMjv5BELj4sa2Ooznrjr8ieE'
+  });
+
+  // image encoding
+  var b64content = fs.readFileSync('public/imgs/image.png', { encoding: 'base64' })
+
+  // first we must post the media to Twitter
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+
+    // now we can reference the media and post a tweet (media will attach to the tweet)
+    var mediaIdStr = data.media_id_string
+    var params = { status: 'intruder captured', media_ids: [mediaIdStr] }
+
+    T.post('statuses/update', params, function (err, data, response) {
+      if (!err && response.statusCode == 200) {
+        res.render('captured', { title: 'Captured' });
+      }
+      else {
+        response.send(err);
+      }
+    })
+  })
+});
+
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
